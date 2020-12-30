b8c25aebffbe5fc8357e0c65e1c16226f55d833a
Yoan Tournade <y@mandelbro.com>
2020-12-30 19:29:07 +0100

    Fix rsync

--- cron/sync_mirror.sh
+++ cron/sync_mirror.sh
@@ -9,6 +9,6 @@ rsync -vaz --chmod='go+rX,go-w' \
     --skip-compress=7z/ace/apk/avi/bz2/deb/flac/gpg/gz/iso/jar/jpeg/jpg/lz/lz4/lzma/lzo/mkv/mov/mp3/mp4/odb/odf/odg/odi/odm/odp/ods/odt/ogg/ogv/opus/otg/oth/otp/ots/ott/oxt/png/rar/rpm/rz/rzip/squashfs/sxc/sxd/sxg/sxm/sxw/tbz/tgz/tlz/txz/tzo/webm/webp/xz/z/zip/zst \
     --delete-delay \
     --delay-updates \
-    rsync://$RSYNC_MIRROR
+    rsync://$RSYNC_MIRROR \
     $RSYNC_TO_DIR
 echo "Synching CTAN mirror from $RSYNC_MIRROR: done! (@`date`)"
